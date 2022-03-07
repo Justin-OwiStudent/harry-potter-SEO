f547c34215654cdd04e071d79e235f09da110882
Justin-OwiStudent <200100@virtualwindow.co.za>
2022-03-07 10:52:38 +0200

    changed fonts and a form
    i added some font chnages to all the pages aswell as added questions to the quiz section,s til to be defined, i also added a basic form layout to the feedback page.

--- JS/Quiz.js
+++ JS/Quiz.js
@@ -31,6 +31,12 @@ $(document).ready(function(){
         }
        
     });
+
+    const API3 = 'http://hp-api.herokuapp.com/api/characters';
+    
+    $.getJSON(API3, function(result){
+    console.log(result);
+    });
 })
 
 
---
+++ JS/main.js
@@ -0,0 +1,5 @@
+const API3 = 'http://hp-api.herokuapp.com/api/characters';
+    
+$.getJSON(API3, function(result){
+    console.log(result);
+});
